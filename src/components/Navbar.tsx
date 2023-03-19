21b8adbf71baaba3730325815dd9683c64d4e219
原貴悌 <haratakayasu@raisondetre.tokyo>
2023-03-20 01:34:57 +0900

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {AiOutlineSearch, AiOutlineClose} from 'react-icons/ai';
+import {TiMicrophone} from 'react-icons/ti'
+import {BsYoutube, BsCameraVideo, BsBell} from 'react-icons/bs'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {IoAppsSharp} from 'react-icons/io5'
+
+
+function Navbar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      dfdfd
+    </div>
+  )
+}
+
+export default Home
